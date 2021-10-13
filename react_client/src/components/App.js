568de5e77197b466c90d0a22763e5bfeb87eda9f
Alex MacLure <almaclure@gmail.com>
2021-10-13 19:57:05 -0400

    Create basic chessboard

--- react_client/src/components/App.js
+++ react_client/src/components/App.js
@@ -1,13 +1,25 @@
 import './App.css';
 import React from "react";
+import Header from './Header';
 import ChessBoard from './ChessBoard';
 
 function App() {
   return (
     <div className="App">
-
-      <ChessBoard/>
-      
+      <section className="header">
+        <Header/>
+      </section>
+      <main className="chat-and-board">
+        <section className="board">
+          <ChessBoard/>
+        </section>
+        <section className="dead-pieces">
+          DEAD PIECES GO HERE
+        </section>
+        <section className="chat"> 
+          CHAT GOES HERE
+        </section>
+      </main>
     </div>
   );
 }
--- react_client/src/components/ChessBoard.js
+++ react_client/src/components/ChessBoard.js
@@ -1,11 +1,23 @@
 import React from "react";
+import "./ChessBoard.css";
 
 export default function ChessBoard() {
 
+  const chessSquares = [];
+
+  for(let i = 0; i < 32; i++) {
+
+    if (i < 4 || i > 7 && i < 12 || i > 15 && i < 20 || i > 23 && i < 28) {
+      chessSquares.push(<div className="square white"></div>, <div className="square black"></div>)
+    } else {
+      chessSquares.push(<div className="square black"></div>, <div className="square white"></div>)
+    }
+  }
+
   return (
-    <main className="chessboard">
-      I AM A CHESS BOARD
-    </main>
+    <div className="chessboard">
+      {chessSquares}
+    </div>
   );
 }
 
