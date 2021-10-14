9851d7fc971ee189284d808758f6f99bff46942b
Alex MacLure <almaclure@gmail.com>
2021-10-14 19:46:35 -0400

    Add piece drag event handlers

--- react_client/src/components/ChessBoard.js
+++ react_client/src/components/ChessBoard.js
@@ -18,6 +18,8 @@ export default function ChessBoard() {
   // ♞ KNIGHT
   // ♟ PAWN
 
+ 
+
   //Easier way to refer to the pieces in code. It's hard to read the unicode character as it's so small
   const pieces = {
     wk: "♔",
@@ -45,7 +47,27 @@ export default function ChessBoard() {
     pieces.br, pieces.bn, pieces.bb, pieces.bq, pieces.bk, pieces.bb, pieces.bn, pieces.br
   ];
 
+  /////---------- use setBoard with FEN notation -------------/////
   const [board, setBoard] = useState(initialState);
+
+  const handleDragStart = (e) => {
+    //e.target.lastChild.data gives the piece value
+    console.log("I AM started dragging", e.target.lastChild.data);
+  }
+
+  const handleDragEnd = (e) => {
+    //e.target.lastChild.data gives the piece value
+    console.log("I AM ended dragging", e.target.lastChild.data);
+  }
+
+  const handleMouseEnter = (e) => {
+    //e.target.lastChild.data gives the piece value
+    console.log("I AM BEING entered", e.target);
+  }
+
+
+
+
 
   //Add white or black squares to the board
   let chessSquares = board.map((piece, i) => {
@@ -53,15 +75,15 @@ export default function ChessBoard() {
     //Alternate colors based on rank
     if (i < 8 || (i > 15 && i < 24) || (i > 31 && i < 40) || (i > 47 && i < 56)) {
       if (i % 2 === 0 ) {
-        return <div className="square white">{piece}</div>
+        return <div className="square white" onMouseEnter={handleMouseEnter} ><span className="piece" draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}>{piece}</span></div>
       } else {
-        return <div className="square black">{piece}</div>
+        return <div className="square black" onMouseEnter={handleMouseEnter} ><span className="piece" draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}>{piece}</span></div>
       };
     } else {
       if (i % 2 === 0 ) {
-        return <div className="square black">{piece}</div>
+        return <div className="square black" onMouseEnter={handleMouseEnter} ><span className="piece" draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}>{piece}</span></div>
       } else {
-        return <div className="square white">{piece}</div>
+        return <div className="square white" onMouseEnter={handleMouseEnter} ><span className="piece" draggable onDragStart={handleDragStart} onDragEnd={handleDragEnd}>{piece}</span></div>
       };
     };
   })
